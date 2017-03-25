de0fc3d37258cac67369bca151edde7c975062df
noonbiteun <zf1228zf@icloud.com>
2017-03-26 01:22:05 +0800

    init master commit

---
+++ IndoorLocation/app/src/main/java/csuft/ppx/indoorlocation/MainActivity.java
@@ -0,0 +1,13 @@
+package csuft.ppx.indoorlocation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
